45545af85f46675f902d3f46e8c31df61673376f
Scott Rowell <rowell.scott@gmail.com>
2018-04-03 15:53:02 -0400

    Implemented Craigslist API

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const craigslist =require('node-craigslist');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  let client = new craigslist.Client({
+    city : 'Miami'
+  });
+
+  client
+  .search('xbox one')
+  .then((listings) => {
+    // play with listings here...
+    listings.forEach((listing) => console.log(listing));
+    res.render('index', { title: 'Craigslist Helper', listings: listings});
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+  
 });
 
 module.exports = router;
