4be5e5025e5d1ea6056b5830bd092fc0f52b694f
Scott Rowell <rowell.scott@gmail.com>
2018-04-23 23:10:09 -0400

    Added Post Details Function From API

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,33 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const craigslist =require('node-craigslist');
-
+const craigslist = require("node-craigslist");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-
+router.get("/", function(req, res, next) {
   let client = new craigslist.Client({
-    city : 'Miami'
+    city: "Miami"
   });
 
   client
-  .search('xbox one')
-  .then((listings) => {
-    // play with listings here...
-    listings.forEach((listing) => console.log(listing));
-    res.render('index', { title: 'Craigslist Helper', listings: listings});
-  })
-  .catch((err) => {
-    console.error(err);
-  });
-  
+    .list()
+    .then(listings => {
+      // play with listings here...
+      listings.forEach(listing => console.log(listing));
+      res.render("index", { title: "Craigslist Helper", listings: listings });
+    })
+    .catch(err => {
+      console.error(err);
+    });
+
+  client
+    .list()
+    .then(listings => client.details(listings[0]))
+    .then(details => {
+      console.log(details);
+    })
+    .catch(err => {
+      console.error(err);
+    });
 });
 
 module.exports = router;
